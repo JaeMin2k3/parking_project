09c667384b31c41eb020ee41c6ed3c53165d63d5
JaeMin <minhtd2k3@gmail.com>
2025-11-02 09:43:48 +0700

    'updateModle'

--- models/ParkingRate.js
+++ models/ParkingRate.js
@@ -3,7 +3,6 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   const ParkingRate = sequelize.define('ParkingRate', {
     id: { type: DataTypes.BIGINT.UNSIGNED, autoIncrement: true, primaryKey: true },
-    plan_type: { type: DataTypes.ENUM('HOURLY','DAILY','WEEKLY','MONTHLY'), allowNull: false },
     vehicle_type: { type: DataTypes.ENUM('CAR','MOTORBIKE'), allowNull: false, defaultValue: 'CAR' },
     currency: { type: DataTypes.CHAR(3), allowNull: false, defaultValue: 'VND' },
     unit_price: { type: DataTypes.DECIMAL(12,2), allowNull: false },
--- models/Ticket.js
+++ models/Ticket.js
@@ -3,18 +3,14 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   const Ticket = sequelize.define('Ticket', {
     id: { type: DataTypes.BIGINT.UNSIGNED, autoIncrement: true, primaryKey: true },
-    spot_id: {
-      type: DataTypes.STRING(8),   // đủ cho A1234, B12...
-      primaryKey: true,
-      allowNull: false,
-      validate: { is: /^[A-Z]\d{1,4}$/ } // 1 chữ A-Z + 1-4 số (điều chỉnh theo nhu cầu)
-    },
+    spot_id: { type: DataTypes.BIGINT.UNSIGNED, allowNull: false },
     reservation_id: { type: DataTypes.BIGINT.UNSIGNED, allowNull: true, unique: true }, // 1 reservation -> tối đa 1 ticket
     plate: { type: DataTypes.STRING(20), allowNull: false },
     vehile_type: { type: DataTypes.ENUM('CAR','MOTORBIKE'), allowNull: false },
     booked_start: { type: DataTypes.DATE, allowNull: true },
     booked_end: { type: DataTypes.DATE, allowNull: true },
     start_time: { type: DataTypes.DATE, allowNull: true },
+    status: {type: DataTypes.BOOLEAN, allowNull: false, default: false}
   }, {
     tableName: 'tickets',
   });
